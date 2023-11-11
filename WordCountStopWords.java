e38e1d1d931e2cba23a2bb26b8152ebd0dda57e2
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-11-10 21:19:05 -0500

    Create WordCountStopWords.java

---
+++ WordCountStopWords.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCountStopWords {
+
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private Set<String> stopWords = new HashSet<>();
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        // Read stop words from the configuration
+        String[] stopWordsArray = context.getConfiguration().get("stop.words").split(",");
+        stopWords.addAll(Arrays.asList(stopWordsArray));
+    }
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            String currentWord = itr.nextToken().toLowerCase();
+            if (!stopWords.contains(currentWord)) {
+                word.set(currentWord);
+                context.write(word, one);
+            }
+        }
+    }
+  }
+
+  public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    
+    // Add stop words to the configuration
+    conf.set("stop.words", "a,an,the,and,or,but,..."); // Add all stop words here
+
+    Job job = Job.getInstance(conf, "word count with stop words");
+    job.setJarByClass(WordCountStopWords.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
