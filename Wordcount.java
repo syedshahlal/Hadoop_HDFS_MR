156b495e2c0d1105cf4651b3941f46fe41ebe053
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-11-10 21:12:07 -0500

    Update Wordcount.java

--- Wordcount.java
+++ Wordcount.java
@@ -14,71 +14,20 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordCount {
 
   public static class TokenizerMapper
-         extends Mapper<Object, Text, Text, IntWritable> {
-  
-      static enum CountersEnum { INPUT_WORDS }
-  
-      private final static IntWritable one = new IntWritable(1);
-      private Text word = new Text();
-  
-      private boolean caseSensitive;
-      private Set<String> patternsToSkip = new HashSet<String>();
-  
-      private Configuration conf;
-      private BufferedReader fis;
-  
-      @Override
-      public void setup(Context context) throws IOException,
-          InterruptedException {
-        conf = context.getConfiguration();
-        caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
-        if (conf.getBoolean("wordcount.skip.patterns", false)) {
-          URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
-          for (URI patternsURI : patternsURIs) {
-            Path patternsPath = new Path(patternsURI.getPath());
-            String patternsFileName = patternsPath.getName().toString();
-            parseSkipFile(patternsFileName);
-          }
-        }
-      }
-  
-      private void parseSkipFile(String fileName) {
-        try {
-          fis = new BufferedReader(new FileReader(fileName));
-          String pattern = null;
-          while ((pattern = fis.readLine()) != null) {
-            patternsToSkip.add(pattern);
-          }
-        } catch (IOException ioe) {
-          System.err.println("Caught exception while parsing the cached file '"
-              + StringUtils.stringifyException(ioe));
-        }
-      }
-  
-      @Override
-      public void map(Object key, Text value, Context context
-                      ) throws IOException, InterruptedException {
-        String line = (caseSensitive) ?
-            value.toString() : value.toString().toLowerCase();
-        for (String pattern : patternsToSkip) {
-          line = line.replaceAll(pattern, "");
-        }
-  
-        // Replace all non-word characters and numbers with spaces
-        line = line.replaceAll("[^a-zA-Z ]", "");
-  
-        StringTokenizer itr = new StringTokenizer(line);
-        while (itr.hasMoreTokens()) {
-          word.set(itr.nextToken());
-          context.write(word, one);
-          Counter counter = context.getCounter(CountersEnum.class.getName(),
-              CountersEnum.INPUT_WORDS.toString());
-          counter.increment(1);
-        }
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
       }
     }
-
-
+  }
 
   public static class IntSumReducer
        extends Reducer<Text,IntWritable,Text,IntWritable> {
